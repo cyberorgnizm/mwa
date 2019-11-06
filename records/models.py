ff73dd3851ba6f4eeaaabfd530517d9aaf8c0652
cyberorgnizm <samuelboss360@gmail.com>
2019-11-06 16:59:01 +0100

    :card_file_box: Create record models

--- records/models.py
+++ records/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class BloodRecord(models.Model):
+    BLOOD_GROUP_OPTION = (
+        ("O+", "O(+) Positive",),
+        ("O-", "O(-) Negative",),
+        ("A+", "A(+) Positive",),
+        ("A-", "O(-) Negative",),
+        ("AB+", "AB(+) Positive",),
+        ("AB-", "AB(-) Negative",)
+    )
+
+    BLOOD_GENOTYPE_OPTION = (
+        ("AA", 'AA',),
+        ("AO", 'AO',),
+        ("AB", 'AB',),
+        ("BO", 'BO',),
+        ("BB", 'BB',),
+        ("OO", 'OO',),
+    )
+
+    profile = models.OneToOneField(
+        'profiles.Patient', 
+        on_delete=models.CASCADE,
+    )
+
+    blood_group = models.CharField(max_length=3)
+    blood_genotype = models.CharField(max_length=2)
+
+
+
+class MeasurementRecord(models.Model):
+    profile = models.OneToOneField(
+        'profiles.Patient', 
+        on_delete=models.CASCADE,
+    )
+
+    height = models.DecimalField(verbose_name="height (m/cm)", max_digits=10, decimal_places=2)
+    weight = models.DecimalField(verbose_name="weight (kg)", max_digits=10, decimal_places=2)
