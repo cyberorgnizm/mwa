1ff2f702fbfb410ad9ccfa3f25b628b9ee31f3b3
cyberorgnizm <samuelboss360@gmail.com>
2019-11-06 13:45:53 +0100

    :camera_flash: Create template and views for profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from profiles.forms import CustomUserCreationForm
 
-# Create your views here.
+class SignUpView(CreateView):
+    """Generic class that implements form rendering and model creation"""
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "signup.html"
