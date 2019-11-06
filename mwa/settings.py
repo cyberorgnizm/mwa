a5b5ed7d752caebc33f350ab295d91638832b375
cyberorgnizm <samuelboss360@gmail.com>
2019-11-06 13:44:26 +0100

    :wrench: Configure settings and URLConf

--- mwa/settings.py
+++ mwa/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mwa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+# Authentication redirection for auth users
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
