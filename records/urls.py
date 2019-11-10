e889098f569d54cefc0d221c8a4e9f6682c7e6f4
Adole Samuel <cyberorgnizm@gmail.com>
2019-11-10 15:31:09 +0100

    :lipstick: Update dahsboard UI

--- records/urls.py
+++ records/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from records.views import StatisticsReportView, MedicalReportsView
 
+
+app_name = "records"
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="records/dashboard.html"), name="dashboard"),
     path("statistics/", StatisticsReportView.as_view(), name="statistics",),
     path("medical/", MedicalReportsView.as_view(), name="records",),
 ]
