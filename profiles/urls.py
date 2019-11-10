458a6b543a9c7016278ee8e79093b7ddd0a49290
Adole Samuel <cyberorgnizm@gmail.com>
2019-11-10 01:13:03 +0100

    :lipstick: Create respective auth view template UI

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from profiles.views import SignUpView, StaffSignUpView
 
-app_label="profiles"
+app_name="profiles"
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('staff/signup/', StaffSignUpView.as_view(), name='staff-signup'),
